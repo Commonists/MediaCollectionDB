f1710e7530cb40da2034b0bd9459440a69bc51c4
Pierre-Selim <pierre-selim.huard@wikimedia.fr>
2015-03-21 14:46:25 +0100

    adding media module

---
+++ mediacollectiondb/media.py
@@ -0,0 +1,33 @@
+""" mediacollectiondb database define the media collection datbase
+and provides tools to manage the database."""
+import sqlite3
+
+
+class MediaCollection(object):
+
+    """ Persistent media collection, i.e. collection saved into
+    SQLite database.
+
+    Attributes:
+        connection: sqlite connection.
+        cursor: sqlite cursor.
+    """
+
+    COLLECTIONS_TABLE = "media_collections"
+
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename)
+        self.cursor = self.connection.cursor()
+
+    def add(self, media):
+        """ Add media to the persistent media collection.
+
+        Args:
+            media: media object to save to the into the persistent collection.
+        """
+        pass
+
+    def reset(self):
+        """ Delete all entry in the database. """
+        reset_query = "DELETE * FROM %s" % MediaCollection.COLLECTIONS_TABLE
+        self.cursor.execute(reset_query)

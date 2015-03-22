dd803222522cf49ee6274d39f649459a0bf442ff
PierreSelim <pierre-selim.huard@wikimedia.fr>
2015-03-23 00:36:42 +0100

    adds caching for listall() adds vicount metric

--- mediacollectiondb/metrics.py
+++ mediacollectiondb/metrics.py
@@ -12,20 +12,30 @@ class MediaCollectionsMetrics(object):
     def __init__(self, collections):
         """ Constructor. """
         self.collections = collections
+        self.cache_allmedia = None
 
     def listall(self):
-        """ List allmedia Media objects from the list of Media Collections. """
-        allmedia = []
-        for collection in self.collections:
-            allmedia.append(collection.listall())
-        return allmedia
+        """ List allmedia Media objects from the list of the set
+        of media collections. """
+        if self.cache_allmedia is None:
+            self.cache_allmedia = []
+            for collection in self.collections:
+                self.cache_allmedia.append(collection.listall())
+        # else returns the cache value
+        return self.cache_allmedia
 
     def mediacount(self):
         """ Return the mediacount metric. """
         return len(self.listall())
 
     def qicount(self):
-        """ Return the qicount metric which represents the amount of 
-        quality images in the set of collection."""
+        """ Return the qicount metric which represents the amount of
+        quality images in the set of collections."""
         qualityimages = [qi for qi in self.listall() if qi.quality_image]
         return len(qualityimages)
+
+    def vicount(self):
+        """ Return the vicount metric which counts the amount of valued images
+        in the set of collections. """
+        valuedimages = [vi for vi in self.listall() if vi.valued_images]
+        return len(valuedimages)

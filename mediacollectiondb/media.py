c310d6aaa5fee1b11bd5cfa63a8a73a6136658cc
Pierre-Selim <pierre-selim.huard@wikimedia.fr>
2015-03-22 10:50:12 +0100

    commiting wip

--- mediacollectiondb/media.py
+++ mediacollectiondb/media.py
@@ -1,5 +1,7 @@
 """ mediacollectiondb database define the media collection datbase
-and provides tools to manage the database."""
+and provides tools to manage the database.
+"""
+
 import sqlite3
 
 
@@ -19,14 +21,14 @@ class MediaCollection(object):
         self.connection = sqlite3.connect(filename)
         self.cursor = self.connection.cursor()
         table_creation = """CREATE TABLE IF NOT EXISTS %s
-                                (pageid, 
+                                (pageid,
                                 filename,
                                 uploader,
                                 width,
                                 height,
                                 size,
                                 quality_image,
-                                featured_pictured,
+                                featured_picture,
                                 valued_image,
                                 timestamp)
                         """ % MediaCollection.COLLECTIONS_TABLE
@@ -39,6 +41,20 @@ class MediaCollection(object):
         Args:
             media: media object to save to the into the persistent collection.
         """
+        pass
+
+    def _exists(self, media):
+        """ Testing whether a media is already in database or not. """
+        self.cursor.execute("""SELECT COUNT(pageid) FROM %s
+            WHERE pageid='%s'
+            """ % (MediaCollection.COLLECTIONS_TABLE, media['pageid']))
+
+    def _update(self, media):
+        """ Updating a media already in database. """
+        pass
+
+    def _insert(self, media):
+        """ Inserting a media into the database. """
         pass
 
     def reset(self):
